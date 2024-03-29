c64be640ead08d5ec305b21424fe4b7c2f1513e5
favycodezz <ezeliorafavour81@gmail.com>
2024-03-28 22:55:11 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Booklist = () => {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => {
+  return (
+    <img
+      src="https://m.media-amazon.com/images/I/81nJUluy7cL._AC_UY218_.jpg"
+      alt="Purple Hibiscus"
+    />
+  );
+};
+
+const Title = () => {
+  return <h2>Purple Hibiscus</h2>;
+};
+
+const Author = () => {
+  return <h2>Chimamanda Ngozi Adichie </h2>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Booklist />);
